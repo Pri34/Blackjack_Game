efd6cb7c8b93cc2536fc5e934314aaa2d78aa9bf
Prisha Aggarwal <prishaagg4@gmail.com>
2024-07-24 12:58:08 +0530

    Developed the game further

--- index.js
+++ index.js
@@ -135,15 +135,123 @@
 //  Store the cards paragraph in a variable called cardsEl
 //  Render the cars on the page using this format -> "Cards: 10 4"
 
+// let firstCard =10
+// let secondCard =11
+// let cards = [firstCard,secondCard]
+// let thirdCard = 4 
+// let sum = firstCard + secondCard 
+// let hasBlackJack = false  
+// isAlive = true
+// let message = ""
+// let messageEl = document.getElementById("message-el")
+// // let sumEl = document.getElementById("sum-el")
+// let sumEl = document.querySelector("#sum-el")
+// let cardsEl = document.querySelector("#cards-el")
+
+// function startGame() {
+//     renderGame()
+// }
+// function renderGame() {
+//     sumEl.textContent = "Sum: " + sum
+//     cardsEl.textContent =  "Cards: " + cards[0] + " " + cards[1]
+//     if(sum <= 20) {
+//         message = "Do you want to draw new card ? "
+//     } else if(sum === 21) {
+//         message = "Wohoo ! you have got Blackjack !"
+//         hasBlackJack = true
+//      } else {
+//         message = "You are out of the game !"
+//         isAlive = false
+//     }
+//     messageEl.textContent = message
+// }
+
+// // 1. Create a card variable, and hard code its value to a number (2-11)
+// // 2. Add the new card to the sum variable
+// // 3. Call startGame()
+
+// function newCard() {
+//     sum += thirdCard
+//     renderGame()
+//     console.log("Draw a new card from deck")
+// }
+
+// // Now we want ki hamara new card display ho jaye along with old cards
+// // We will do that using arrays
+// //its hardcored that only 1st card and 2nd card will display
+// //What we want is to render all the cards present at present moment
+// // we will to store all cards in single variable no matter how many cards we have 
+
+
+
+
+// // LEARNING ARRAYS
+
+
+
+// //Arrays are ordered lists of items
+
+// let featuredPosts = ["Check out my Netflix Clone","Here's the code for my project","I have just relaunched my porfolio"]
+
+// // Create an array that lists your i.e. experience, education, licenses, skills or similar
+// // The items of the array should be strings
+
+// console.log(featuredPosts[2])
+
+// let items = ["I am Studying from Thapar University","I am learning js rn","I can play guitar"]
+
+// // Make the following appear in the console:
+// // Frontend developer at Xeneta
+// // People counter for Norstat
+// // CEO at Scrimba
+
+// console.log(items[1])
+// console.log(items[2])
+// console.log(items[0])
+
+// console.log(items.length)
+
+// // Array - ordered list of items - coposite / complex data type
+
+// // Create an array that describes yourself. Use the three primitive data types you've learned
+// // It should contain your name (string), your age (number), and whether you like pizza (boolean)
+// let info=["Prisha Aggarwal", 19, true]
+
+// //Adding items to end of array
+// let hello= [ 7,4]
+// hello.push(6)
+// console.log(hello)
+
+// // Push the newMessage to the messages array, and then log out the array
+
+// let messages = [
+//     "Hey, how's it going?",        
+//     "I'm great, thank you! How about you?",
+//     "All good. Been working on my portfolio lately."
+// ]
+
+// let newMessage = "Same here!"
+// messages.push(newMessage)
+// console.log(messages)
+
+// messages.pop()
+// console.log(messages)
+
+
+
+//BACK TO THE GAME
+
+
+
+
 let firstCard =10
 let secondCard =11
-let thirdCard = 4 
+let cards = [firstCard,secondCard]
 let sum = firstCard + secondCard 
 let hasBlackJack = false  
 isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-// let sumEl = document.getElementById("sum-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
@@ -152,7 +260,7 @@ function startGame() {
 }
 function renderGame() {
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent =  "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent =  "Cards: " + cards[0] + " " + cards[1]
     if(sum <= 20) {
         message = "Do you want to draw new card ? "
     } else if(sum === 21) {
@@ -165,13 +273,32 @@ function renderGame() {
     messageEl.textContent = message
 }
 
-// 1. Create a card variable, and hard code its value to a number (2-11)
-// 2. Add the new card to the sum variable
-// 3. Call startGame()
-
-function newCard() {
+function newCard() { 
+    let thirdCard = 4 
     sum += thirdCard
+    cards.push(thirdCard)
+    // console.log(cards)
     renderGame()
     console.log("Draw a new card from deck")
 }
+
+
+//LEARNING FOR LOOPS
+
+
+// Count to ten!
+
+// We need to specify...
+
+// Where should we START counting?
+// Where is the FINISH line?
+// What's the STEP SIZE we should use?
+
+//    START           FINISH       STEP SIZE
+for ( let count = 1;  count < 11;  count += 1 )  {
+    
+    console.log(count)
+
+}
+
 

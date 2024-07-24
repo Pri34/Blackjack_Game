e87e2041c37933c29a6e3012fb465011ba127eae
Prisha Aggarwal <prishaagg4@gmail.com>
2024-07-24 23:59:25 +0530

    Generated Random Numbers

--- index.js
+++ index.js
@@ -244,29 +244,336 @@
 
 
 
-let firstCard =10
-let secondCard =11
-let cards = [firstCard,secondCard]
-let sum = firstCard + secondCard 
-let hasBlackJack = false  
-isAlive = true
+// let firstCard =10
+// let secondCard =11
+// let cards = [firstCard,secondCard]
+// let sum = firstCard + secondCard 
+// let hasBlackJack = false  
+// isAlive = true
+// let message = ""
+// let messageEl = document.getElementById("message-el")
+// let sumEl = document.querySelector("#sum-el")
+// let cardsEl = document.querySelector("#cards-el")
+
+// function startGame() {
+//     renderGame()
+// }
+// function renderGame() {
+//     sumEl.textContent = "Sum: " + sum
+//     cardsEl.textContent =  "Cards: " + cards[0] + " " + cards[1]
+//     if(sum <= 20) {
+//         message = "Do you want to draw new card ? "
+//     } else if(sum === 21) {
+//         message = "Wohoo ! you have got Blackjack !"
+//         hasBlackJack = true
+//      } else {
+//         message = "You are out of the game !"
+//         isAlive = false
+//     }
+//     messageEl.textContent = message
+// }
+
+// function newCard() { 
+//     let thirdCard = 4 
+//     sum += thirdCard
+//     cards.push(thirdCard)
+//     // console.log(cards)
+//     renderGame()
+//     console.log("Draw a new card from deck")
+// }
+
+
+// //LEARNING FOR LOOPS
+
+
+// // Count to ten!
+
+// // We need to specify...
+
+// // Where should we START counting?
+// // Where is the FINISH line?
+// // What's the STEP SIZE we should use?
+
+// //    START           FINISH       STEP SIZE
+// for ( let count = 1;  count < 11;  count += 1 )  {
+    
+//     console.log(count)
+
+// }
+
+// // Create a for loop that counts from 10 to 100 in steps of 10
+// // Use console.log to log out the numbers
+// for(let i=10 ; i < 101  ; i+=10 ) {
+//     console.log(i)
+// }
+
+// //COMBINING FOR LOOPS AND ARRAYS
+// let messages = [
+//     "Hey, how's it going?",        
+//     "I'm great, thank you! How about you?",
+//     "All good. Been working on my portfolio lately.",
+//     "Same here!",
+//     "hello",
+//     "me"
+
+// ]
+// for(let j=0 ; j<messages.length; j+=1) { 
+//     console.log(messages[j])
+// }
+
+// let cards1 = [7, 3, 9]
+
+// // Create a for loop that logs out all the cards in the array
+// // Use cards.length to specify how long the loop should run
+
+// for(let i=0 ; i<cards1.length ; i++) { 
+//     console.log(cards1[i])
+// }
+
+// //ARRAYS LOOPS AND DOM
+
+// // let sentence = ["Hello ", "my ", "name ", "is ", "Per"] 
+// // let greetingEl = document.getElementById("greeting-el")
+
+// // // Render the sentence in the greetingEl paragraph using a for loop and .textContent
+// // for(let i=0 ; i<sentence.length ; i++) {
+// //     greetingEl.textContent += sentence[i]
+// // }
+
+// let sentence = ["Hello", "my", "name", "is", "Per"] 
+// let greetingEl = document.getElementById("greeting-el")
+
+// // How do you keep the spaces between the words if I remve them from the array?
+
+// for (let i = 0; i < sentence.length; i++) {
+//     greetingEl.textContent += sentence[i] + " "
+// }
+
+
+
+//BACK TO THE GAME
+
+
+// let firstCard =10
+// let secondCard =11
+// let cards = [firstCard,secondCard]
+// let sum = firstCard + secondCard 
+// let hasBlackJack = false  
+// isAlive = true
+// let message = ""
+// let messageEl = document.getElementById("message-el")
+// let sumEl = document.querySelector("#sum-el")
+// let cardsEl = document.querySelector("#cards-el")
+
+// function startGame() {
+//     renderGame()
+// }
+// function renderGame() {
+//     sumEl.textContent = "Sum: " + sum
+//     cardsEl.textContent =  "Cards: "
+//     for(let i=0 ; i<cards.length ; i++) {
+//         cardsEl.textContent += cards[i] + " "
+//     }
+//     if(sum <= 20) {
+//         message = "Do you want to draw new card ? "
+//     } else if(sum === 21) {
+//         message = "Wohoo ! you have got Blackjack !"
+//         hasBlackJack = true
+//      } else {
+//         message = "You are out of the game !"
+//         isAlive = false
+//     }
+//     messageEl.textContent = message
+// }
+
+// function newCard() { 
+//     let thirdCard = 4 
+//     sum += thirdCard
+//     cards.push(thirdCard)
+//     renderGame()
+// }
+
+
+
+// //Learning to generate random numbers
+
+
+// let player1Time = 102
+// let player2Time = 107
+
+// //find fatest race time
+// function getFastestRaceTime() { 
+//     if(player1Time < player2Time) { 
+//         return player1Time
+//     }
+//     else if(player1Time > player2Time) {
+//         return player2Time
+//     }
+//     else { // if both have same time
+//         return player2Time
+//     }
+// }
+
+// let fastestRace = getFastestRaceTime()
+// console.log(fastestRace)
+
+// // Write a function that returns the total race time
+// // Call/invoke the function and store the returned value in a new variable
+// // Finally, log the variable out
+
+// function totalRaceTime()  {
+//     let time = player1Time + player2Time
+//     return time
+// }
+
+// let totalTime = totalRaceTime()
+// console.log(totalTime)
+
+
+
+
+//BACK TO THE GAME
+
+
+
+// let firstCard =getRandomCard() 
+// let secondCard =getRandomCard() 
+// let cards = [firstCard,secondCard]
+// let sum = firstCard + secondCard 
+// let hasBlackJack = false  
+// isAlive = true 
+// let message = ""
+// let messageEl = document.getElementById("message-el")
+// let sumEl = document.querySelector("#sum-el")
+// let cardsEl = document.querySelector("#cards-el")
+
+
+// function getRandomCard() {
+//     return 5
+// }
+// function startGame() {
+//     renderGame()
+// }
+// function renderGame() {
+//     sumEl.textContent = "Sum: " + sum
+//     cardsEl.textContent =  "Cards: "
+//     for(let i=0 ; i<cards.length ; i++) {
+//         cardsEl.textContent += cards[i] + " "
+//     }
+//     if(sum <= 20) {
+//         message = "Do you want to draw new card ? "
+//     } else if(sum === 21) {
+//         message = "Wohoo ! you have got Blackjack !"
+//         hasBlackJack = true
+//      } else {
+//         message = "You are out of the game !"
+//         isAlive = false
+//     }
+//     messageEl.textContent = message
+// }
+
+// function newCard() { 
+//     let thirdCard = getRandomCard()  
+//     sum += thirdCard
+//     cards.push(thirdCard)
+//     renderGame()
+// }
+
+
+
+// //Random Number Generation
+
+
+
+// let randomNumber = Math.random()*6
+
+// console.log(randomNumber)
+
+
+// /* 
+
+// In which range will our randomNumber be now?
+
+// From: 0 
+// To: 5.999
+ 
+// */
+
+// // let randomNumber = Math.random() * 6
+
+// let flooredNumber = Math.floor(3.45632)
+
+// console.log(flooredNumber)
+
+
+// /* 
+// What does Math.floor() do to positive numbers?
+// */
+
+// let randomNumber1 = Math.floor( Math.random() * 6)
+
+// console.log(randomNumber1)
+
+// /* 
+// Write down all the possible values randomNumber can hold now!
+// 0 1 2 3 4 5
+// */
+
+// // Try to modify the expression so that we get a range from 1 to 6
+
+// function rollDice() { 
+//     let randomNumber2 = Math.floor( Math.random() * 6 ) + 1
+//     return randomNumber2
+// }
+// console.log(rollDice())
+
+
+
+//BACK TO THE GAME
+
+let cards = []
+let sum = 0
+let hasBlackJack = false 
+isAlive = false 
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
-function startGame() {
+//Now if 1 --> return 11
+//Now if 11-13 ---> return 10
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if(randomNumber === 1) {
+        return 11
+    } else if  (randomNumber>10) {
+        return 10
+    } else {
+    return randomNumber
+    }
+}
+function startGame() { //Everything should start working on clicking start button
+    //also person is alive when he hits start button
+    isAlive = true
+    let firstCard =getRandomCard() 
+    let secondCard =getRandomCard() 
+    cards = [firstCard,secondCard]
+    sum = firstCard + secondCard 
     renderGame()
 }
 function renderGame() {
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent =  "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent =  "Cards: "
+    for(let i=0 ; i<cards.length ; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     if(sum <= 20) {
         message = "Do you want to draw new card ? "
     } else if(sum === 21) {
         message = "Wohoo ! you have got Blackjack !"
         hasBlackJack = true
-     } else {
+    } else {
         message = "You are out of the game !"
         isAlive = false
     }
@@ -274,31 +581,52 @@ function renderGame() {
 }
 
 function newCard() { 
-    let thirdCard = 4 
+    let thirdCard = getRandomCard()  
     sum += thirdCard
     cards.push(thirdCard)
-    // console.log(cards)
     renderGame()
-    console.log("Draw a new card from deck")
 }
 
 
-//LEARNING FOR LOOPS
 
+//LOGICAL AND OPERATOR
 
-// Count to ten!
+let hasCompletedCourse = true
+let givesCertificate =true
 
-// We need to specify...
+//We want that function should only work if above two conditions are true
 
-// Where should we START counting?
-// Where is the FINISH line?
-// What's the STEP SIZE we should use?
+if( hasCompletedCourse === true && givesCertificate === true) { 
+generateCertificate()
+}
+function generateCertificate() {
+    console.log("Generating certificate....")
+}
+ 
+let hasSolvedChallenge = false
+let hasHintsLeft =false
 
-//    START           FINISH       STEP SIZE
-for ( let count = 1;  count < 11;  count += 1 )  {
-    
-    console.log(count)
+// Create an if statement that checks that both variables are false.
+// If so, run the showSolution() function
 
+if(hasSolvedChallenge== false && hasHintsLeft==false) {
+    showSolution()
+}
+function showSolution() {
+    console.log("Showing the solution....")
 }
 
+//LOGICAL OR OPERATOR
+
+// Create two boolean variables, likesDocumentaries and likesStartups
+// Use an OR statement (||) to call recommendMovie() if either of those variables are true
+
+let likesDocumentries =  true
+let likesStartups = false
+if(likesDocumentries == true || likesStartups == true) {
+    recommendMovie()
+}
+function recommendMovie() {
+    console.log("Hey, check out this new film we think you will like!")
+}
 
